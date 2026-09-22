a6b7d971dd72b754da5219e1b7afc23b78d65f96
valtecno05-collab <valtecno05@gmail.com>
2026-09-22 19:27:21 +0700

    Delete lib/supabase.ts

--- lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = process.env.https://ypjsrtctfazxlnwcxtyw.supabase.co!;
-const supabaseAnonKey = process.env.sb_publishable_FMKKD-XifaEPuVxI3SJ3UA_7zNPMczD!;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
