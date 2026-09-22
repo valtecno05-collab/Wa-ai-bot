bbbb0f7644f5cc92d23bd0420c37cc25620d4cad
valtecno05-collab <valtecno05@gmail.com>
2026-09-22 19:01:36 +0700

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,3 +4,4 @@ const supabaseUrl = process.env.https://ypjsrtctfazxlnwcxtyw.supabase.co || '';
 const supabaseAnonKey = process.env.sb_publishable_FMKKD-XifaEPuVxI3SJ3UA_7zNPMczD || '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+  
