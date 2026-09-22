470b919848c76e68d267f50e9874f5bf7951cc8c
valtecno05-collab <valtecno05@gmail.com>
2026-09-22 19:28:23 +0700

    Create supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(process.env.https://ypjsrtctfazxlnwcxtyw.supabase.co!, process.env.sb_publishable_FMKKD-XifaEPuVxI3SJ3UA_7zNPMczD!);
